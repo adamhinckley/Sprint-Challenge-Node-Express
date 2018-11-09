49184b53cadaad84f63830e10e3084bea113e7e2
adamhinckley <adamhinckley@mac.com>
2018-11-09 09:27:05 -0700

    inital commit - dependencies installed and get request is working

---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const actions = require("../data/helpers/actionModel");
+const projects = require("../data/helpers/projectModel");
+
+const server = express();
+
+server.use(express.json());
+
+//endpoints for actions
+
+server.get("/actions", (req, res) => {
+  actions
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "the action info could not be received", err });
+    });
+});
+
+server.get("/actions/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("get with ID", req.params);
+  actions
+    .get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({ message: "the action with the specified ID does not exist" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The action info could not be received", err });
+    });
+});
+
+module.exports = server;
